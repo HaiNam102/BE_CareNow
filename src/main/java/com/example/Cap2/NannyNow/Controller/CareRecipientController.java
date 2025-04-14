513894720cb9debf70ecf36b738bc883cc95efb0
HaiNam102 <bimhuongday@gmail.com>
2025-04-14 13:41:16 +0700

    add create and update Recipient

--- src/main/java/com/example/Cap2/NannyNow/Controller/CareRecipientController.java
+++ src/main/java/com/example/Cap2/NannyNow/Controller/CareRecipientController.java
@@ -1,5 +1,6 @@
 package com.example.Cap2.NannyNow.Controller;
 
+import com.example.Cap2.NannyNow.DTO.Request.CareRecipientReq;
 import com.example.Cap2.NannyNow.DTO.Response.ApiResponse;
 import com.example.Cap2.NannyNow.Exception.SuccessCode;
 import com.example.Cap2.NannyNow.Service.CareRecipientService;
@@ -26,6 +27,42 @@ public class CareRecipientController {
                 .code(SuccessCode.GET_SUCCESSFUL.getCode())
                 .message(SuccessCode.GET_SUCCESSFUL.getMessage())
                 .data(careRecipientService.getCareRecipientsByCustomerId(customerId))
+                .build()
+        );
+    }
+    
+    @PostMapping("/customer")
+    public ResponseEntity<?> createCareRecipient(@RequestBody CareRecipientReq careRecipientReq,
+                                                 @RequestHeader("Authorization") String authHeader) {
+        String token = authHeader.replace("Bearer ", "");
+        Long customerId = jwtUtil.extractUserId(token);
+        
+        return ResponseEntity.ok(ApiResponse.builder()
+                .code(SuccessCode.ADD_SUCCESSFUL.getCode())
+                .message(SuccessCode.ADD_SUCCESSFUL.getMessage())
+                .data(careRecipientService.createCareRecipient(careRecipientReq, customerId))
+                .build()
+        );
+    }
+    
+    @PostMapping("/customer/{customerId}")
+    public ResponseEntity<?> createCareRecipientByCustomerId(@RequestBody CareRecipientReq careRecipientReq,
+                                                          @PathVariable Long customerId) {
+        return ResponseEntity.ok(ApiResponse.builder()
+                .code(SuccessCode.ADD_SUCCESSFUL.getCode())
+                .message(SuccessCode.ADD_SUCCESSFUL.getMessage())
+                .data(careRecipientService.createCareRecipient(careRecipientReq, customerId))
+                .build()
+        );
+    }
+    
+    @PutMapping("/{careRecipientId}")
+    public ResponseEntity<?> updateCareRecipient(@PathVariable Long careRecipientId,
+                                                @RequestBody CareRecipientReq careRecipientReq) {
+        return ResponseEntity.ok(ApiResponse.builder()
+                .code(SuccessCode.UPDATE_SUCCESSFUL.getCode())
+                .message(SuccessCode.UPDATE_SUCCESSFUL.getMessage())
+                .data(careRecipientService.updateCareRecipient(careRecipientId, careRecipientReq))
                 .build()
         );
     }
